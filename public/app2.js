b4335d3a0b90d35a33ac1e56f454f87c8c18f392
Zachary Gordon <z.gordon.h@gmail.com>
2015-06-03 14:33:26 -0700

    add firebase data and conpare function

--- public/app2.js
+++ public/app2.js
@@ -1,14 +1,44 @@
-var myFirebaseRef = new Firebase("https://practiceplace.firebaseio.com/");
+$(function(){
 
 
-myFirebaseRef.set({
-  location: {
-    city: "San Francisco",
-    state: "California",
-    zip: 94103
-  }
-});
+  var myFirebaseRef = new Firebase("https://practiceplace.firebaseio.com/");
 
-myFirebaseRef.child("location/state").on("value", function(snapshot) {
-  alert(snapshot.val());
+
+  myFirebaseRef.set({
+    wallingford: {
+      zip: 98103,
+      neighborhood: "Wallingford, WA",
+      cost: 300,
+      time: 4,
+      size: "500 sqft",
+      outlets: 6,
+      contact: "nacy@gmail.com",
+      image: "http://bygghjalphemma.se/wp-content/uploads/2014/11/bill-gates-wealthiest-person.jpg",
+    },
+
+    ballard: {
+
+    }
+
+  });
+
+  myFirebaseRef.on("value", function(snapshot) {
+    var input=document.getElementById('user');
+
+    $('#submit').on('click', function() {
+      if (input.value == snapshot.child("wallingford/zip").val()){
+        var data = snapshot.child("wallingford").val();
+      $('#resultsDisplay1').text(data.zip);
+      $('#resultsDisplay2').text(data.neighborhood);
+      $('#resultsDisplay3').text(data.cost);
+      $('#resultsDisplay4').text(data.time);
+      $('#resultsDisplay5').text(data.size);
+      $('#resultsDisplay6').text(data.outlets);
+      $('#resultsDisplay7').text(data.contact);
+      $('#resultsDisplay8').text(data.image);
+      } else {
+        alert('Invalid Zip Code');
+      }
+    });
+  });
 });
