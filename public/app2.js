80918c794838f357ffb5319c8ce3ee0fae101e16
Zachary Gordon <z.gordon.h@gmail.com>
2015-06-03 09:19:02 -0700

    firebase try

---
+++ public/app2.js
@@ -0,0 +1,15 @@
+var Firebase = require("firebase");
+var myFirebaseRef = Firebase('https://practiceplace.firebaseio.com/')
+
+myFirebaseRef.set({
+  title: "Hello World!",
+  author: "Firebase",
+  location: {
+    city: "San Francisco",
+    state: "California",
+    zip: 94103
+  }
+});
+myFirebaseRef.child("location/city").on("value", function(snapshot) {
+  alert(snapshot.val());
+});
